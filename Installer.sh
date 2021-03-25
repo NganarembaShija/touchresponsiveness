d7223e5cc284fb1bdfb2fdf98705c20e64d6d979
NganarembaShija <76985647+NganarembaShija@users.noreply.github.com>
2021-03-25 19:17:34 +0530

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -1,18 +1,21 @@
+# Installing sudo in Termux
+pkg install tsu -y
+
 # Rem Build.prop Tweaks
 
 #Mounting...
 
-su -c mount -o rw,remount / &> /dev/null
-su -c chmod +x Installer.sh > /dev/null 2>&1
+sudo mount -o rw,remount / &> /dev/null
+sudo chmod +x Installer.sh > /dev/null 2>&1
 
 # Adding build.prop tweaks for Touch responsiveness
 
 # Backing up original build.prop
 
-su -c tee /sdcard/build.prop.backup /system/build.prop.backup < /system/build.prop > /dev/null
+sudo tee /sdcard/build.prop.backup /system/build.prop.backup < /system/build.prop > /dev/null
 
 # Applying Tweaks
 
-su -c cat build.prop >> /system/build.prop
+sudo cat build.prop >> /system/build.prop
 
-echo -e "Done!\e[1;91mPlease Reboot your phone\e[0m \n\n"
+echo -e "Done!\e[1;91m Please Reboot your phone\e[0m \n\n"
