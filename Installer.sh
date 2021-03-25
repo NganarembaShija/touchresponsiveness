de130319c8e4c0c6a04390a5bc5691739ce508aa
NganarembaShija <76985647+NganarembaShija@users.noreply.github.com>
2021-03-25 17:57:03 +0530

    Create Installer.sh

---
+++ Installer.sh
@@ -0,0 +1,18 @@
+# Rem Build.prop Tweaks
+
+#Mounting...
+
+su -c mount -o rw,remount /
+su -c chmod +x Installer.sh > /dev/null
+
+# Adding build.prop tweaks for Touch responsiveness
+
+# Backing up original build.prop
+
+su -c tee /sdcard/build.prop.backup /system/build.prop.backup < /system/build.prop > /dev/null
+
+# Applying Tweaks
+
+su -c cat build.prop >> /system/build.prop
+
+echo -e "Done!\e[1;91mPlease Reboot your phone\e[0m \n\n"
